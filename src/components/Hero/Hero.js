02358074f1b2565e14be9471d5e7ab60fe161665
Wishnu22 <vishnuunnikrishnan594@gmail.com>
2023-01-05 09:58:03 +0530

    updated

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,7 +13,7 @@ function Hero() {
         <div className='col-md-2'></div>
         </div>
         <div className='row mt-5'>
-            <div className='col-md-1'>
+            <div className='col-md-1 descri'>
             Zelgry is one of the largest international fashion brands known for its trendy 
 fashion wear. Founded a group of indian guys in the year 1994, the italian brand has established itself 
 as a premium luxury brand that offers cutting-edge styles for men, women and kids.
@@ -24,7 +24,7 @@ as a premium luxury brand that offers cutting-edge styles for men, women and kid
         <img src={require("../../Images/hero.jpg")} alt="heroinlogo" className='img-fluid heroin'/>
         
         </div>
-        <div className='col-md-1'>The men’s collection includes jackets & coats, trousers, sweatshirts, footwear, 
+        <div className='col-md-1 descri'>The men’s collection includes jackets & coats, trousers, sweatshirts, footwear, 
 & more fashion accessories. The women can shop for coats, jackets, blazers, jumpsuits, 
 shirts & tops, trousers & jeans, skirts & shorts, shoes & bags. Delivering an extensive 
 range of latest fashion trends, Zelgry specializes in fashion accessories, swimwear,
